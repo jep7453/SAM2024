35259082a0d343da8709aad11eb9e9a7a74ee057
Parva Shah <parvashah310501@gmail.com>
2023-11-27 18:40:25 -0500

    abc

---
+++ Client/src/main/java/com/View/SubmitterUi.java
@@ -0,0 +1,5 @@
+package com.View;
+
+public class SubmitterUi extends View {
+
+}
---
+++ Client/src/main/java/com/View/View.java
@@ -0,0 +1,11 @@
+package com.View;
+
+public class View {
+    public int login(String id,String pass){
+
+    }
+
+    public void home(){
+
+    }
+}
